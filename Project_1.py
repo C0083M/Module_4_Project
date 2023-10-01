b4f4445ca2f058efc4fd4a8a52ed22280432ea44
C0083M <144258256+C0083M@users.noreply.github.com>
2023-10-01 18:11:16 -0500

    Update Project_1.py

--- Project_1.py
+++ Project_1.py
@@ -1,23 +1,29 @@
 import requests
 import json
 
-def get_ip_info():
-    try:
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
 
-        response = requests.get('https://ipinfo.io')
+#api_token = "76c0d7790488e"
+
+    def get_ip_info():
+        try:
         
-        if response.status_code == 200:
-            data = response.json()
+            response = requests.get('https://ipinfo.io')
+        
+            if response.status_code == 200:
+                data = response.json()
             
-            ipv4 = data.get('ip')
-            ipv6 = data.get('ip6')
+                ipv4 = data.get('ip')
+                ipv6 = data.get('ip6')
 
-            print(f'IPv4 Address: {ipv4}')
-            print(f'IPv6 Address: {ipv6}')
-        else:
-            print(f'Error: {response.status_code}')
-    except Exception as e:
-        print(f'Error: {str(e)}')
+                print(f'IPv4 Address: {ipv4}')
+                print(f'IPv6 Address: {ipv6}')
+            else:
+                print(f'Error: {response.status_code}')
+        except Exception as e:
+            print(f'Error: {str(e)}')
 
-if __name__ == '__main__':
-    get_ip_info()
+    if __name__ == '__main__':
+        get_ip_info()
