43ef9b1959899655b7de9eb15ee6c11bfa9cda55
C0083M <melsonc0083@my.uwstout.edu>
2023-10-02 18:57:41 +0000

    api

--- api/index.py
+++ api/index.py
@@ -1,33 +1,30 @@
 import requests
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
+from typing import Dict
 
-class IPCollectorHandler(BaseHTTPRequestHandler):
+def get_ip_info() -> Dict[str, str]:
+    try:
+        response = requests.get('https://ipinfo.io')
+        if response.status_code == 200:
+            data = response.json()
+            ipv4 = data.get('ip')
+            ipv6 = data.get('ip6')
+            return {
+                "IPv4 Address": ipv4,
+                "IPv6 Address": ipv6
+            }
+        else:
+            return {
+                "Error": f"HTTP Error {response.status_code}"
+            }
+    except Exception as d:
+        return {
+            "Error": str(d)
+        }
 
-    def get_ip_info(self):
-        try:
-            response = requests.get('https://ipinfo.io')
-            if response.status_code == 200:
-                data = response.json()
-                ipv4 = data.get('ip')
-                ipv6 = data.get('ip6')
-                return f'IPv4 Address: {ipv4}\nIPv6 Address: {ipv6}'
-            else:
-                return f'Error: {response.status_code}'
-        except Exception as d:
-            return f'Error: {str(d)}'
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        response = self.get_ip_info()
-        self.wfile.write(response.encode())
-
-def run_server():
-    server_address = ("0.0.0.0", 3000)
-    httpd = HTTPServer(server_address, IPCollectorHandler)
-    print("Server is running on port 3000")
-    httpd.serve_forever()
-
-if __name__ == "__main__":
-    run_server()
+def handler(event, context):
+    ip_info = get_ip_info()
+    return {
+        "statusCode": 200,
+        "body": ip_info
+    }
