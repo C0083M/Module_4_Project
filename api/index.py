75f237b8c0eec60d49acd12592af6dedd591e33e
C0083M <melsonc0083@my.uwstout.edu>
2023-10-02 19:12:12 +0000

    api

--- api/index.py
+++ api/index.py
@@ -1,35 +1,50 @@
+# Need requests to use the API, need BaseHTTPRequestHandler to use vercel serverless function
 import requests
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
+# Class handler invokes a serverless function in vercel. I had to read up on their documentation to figure this out
 class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            ip_info = self.get_public_ip_info()
+            self.wfile.write(ip_info.encode())
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Not Found".encode())
+   
+    # Our original IP function from part 1
+    def get_public_ip_info(self):
+        url = "http://ip-api.com/json/"
 
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            ip_info = response.json()
 
-        def get_ip_info():
-            try:
+            if ip_info["status"] == "success":
+                result = "Public IP Address Information:\n"
+                result += f"IP Address: {ip_info['query']}\n"
+                result += f"City: {ip_info['city']}\n"
+                result += f"Region: {ip_info['regionName']}\n"
+                result += f"Country: {ip_info['country']}\n"
+                result += f"ISP: {ip_info['isp']}"
+                return result
+            else:
+                return "Failed to retrieve IP information."
 
-                response = requests.get('https://ipinfo.io')
+        except requests.exceptions.RequestException as e:
+            return f"Error: {e}"
 
-                if response.status_code == 200:
-                    data = response.json()
-
-                    ipv4 = data.get('ip')
-                    ipv6 = data.get('ip6')
-
-                    print(f'IPv4 Address: {ipv4}')
-                    print(f'IPv6 Address: {ipv6}')
-                else:
-                    print(f'Error: {response.status_code}')
-            except Exception as d:
-                print(f'Error: {str(d)}')
-
-        if __name__ == '__main__':
-            get_ip_info()
-
-def server():
+# The serverless function definition and function itself
+def run_vercel_server():
     server_address = ("0.0.0.0", 3000)
     httpd = HTTPServer(server_address, IPCollectorHandler)
-    httpd.server()
+    httpd.serve_forever()
 
-    if __name__ == "__main__":
-        server()
+if __name__ == "__main__":
+    run_vercel_server()
