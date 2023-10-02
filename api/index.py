41befd3a297470b5a64c733b200b3e8e1334c223
C0083M <melsonc0083@my.uwstout.edu>
2023-10-02 19:22:14 +0000

    api

--- api/index.py
+++ api/index.py
@@ -1,21 +1,16 @@
 import requests
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class CustomHandler(BaseHTTPRequestHandler):
-    def custom_request_handler(self):
-        if self.path == "/mypath":
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            ip_info = self.retrieve_custom_ip_info()
+            ip_info = self.get_public_ip_info()
             self.wfile.write(ip_info.encode())
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write("Resource Not Found".encode())
    
-    def retrieve_custom_ip_info(self):
+    def get_public_ip_info(self):
         url = "http://ip-api.com/json/"
 
         try:
@@ -24,23 +19,19 @@ class CustomHandler(BaseHTTPRequestHandler):
             ip_info = response.json()
 
             if ip_info["status"] == "success":
-                result = "Public IP Information:\n"
+                result = "Public IP Address Information:\n"
                 result += f"IP Address: {ip_info['query']}\n"
-                result += f"City: {ip_info['city']}\n"
-                result += f"Region: {ip_info['regionName']}\n"
-                result += f"Country: {ip_info['country']}\n"
-                result += f"ISP: {ip_info['isp']}"
                 return result
             else:
-                return "Failed to fetch IP information."
+                return "Error"
 
         except requests.exceptions.RequestException as e:
             return f"Error: {e}"
 
-def run_custom_server():
+def run_vercel_server():
     server_address = ("0.0.0.0", 3000)
-    httpd = HTTPServer(server_address, CustomHandler)
+    httpd = HTTPServer(server_address, IPCollectorHandler)
     httpd.serve_forever()
 
 if __name__ == "__main__":
-    run_custom_server()
+    run_vercel_server()
