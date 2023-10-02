bc17da92e87145268c6fdd0d7f7c74e3fc910b4b
C0083M <melsonc0083@my.uwstout.edu>
2023-10-02 03:38:06 +0000

    api

--- api/index.py
+++ api/index.py
@@ -1,33 +1,38 @@
 import requests
-import json
-{
- "ipinfo": {
-        "api_key": "76fc0d7790488e"
-}
-}
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class handler(BaseHTTPRequestHandler):
+class IPCollectorHandler(BaseHTTPRequestHandler):
 
-    #api_token = "76c0d7790488e"
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            ip_info = self.get_public_ip_info()
+            self.wfile.write(ip_info.encode())
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Not Found".encode())
 
-        def get_ip_info():
-            try:
+    def get_public_ip_info(self):
+        try:
+            response = requests.get('https://ipinfo.io')
+            if response.status_code == 200:
+                data = response.json()
+                ipv4 = data.get('ip')
+                ipv6 = data.get('ip6')
+                return f'IPv4 Address: {ipv4}\nIPv6 Address: {ipv6}'
+            else:
+                return f'Error: {response.status_code}'
+        except Exception as e:
+            return f'Error: {str(e)}'
 
-                response = requests.get('https://ipinfo.io')
+def run_vercel_server():
+    server_address = ("0.0.0.0", 3000)
+    httpd = HTTPServer(server_address, IPCollectorHandler)
+    httpd.serve_forever()
 
-                if response.status_code == 200:
-                    data = response.json()
-
-                    ipv4 = data.get('ip')
-                    ipv6 = data.get('ip6')
-
-                    print(f'IPv4 Address: {ipv4}')
-                    print(f'IPv6 Address: {ipv6}')
-                else:
-                    print(f'Error: {response.status_code}')
-            except Exception as e:
-                print(f'Error: {str(e)}')
-
-        if __name__ == '__main__':
-            get_ip_info()
+if __name__ == "__main__":
+    run_vercel_server()
