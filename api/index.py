5ab59409da117e1599a2926db5a5122b11cf52ec
C0083M <melsonc0083@my.uwstout.edu>
2023-10-02 18:54:42 +0000

    api

--- api/index.py
+++ api/index.py
@@ -1,37 +1,33 @@
 import requests
-import json
-
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+class IPCollectorHandler(BaseHTTPRequestHandler):
 
-    #api_token = "76c0d7790488e"
+    def get_ip_info(self):
+        try:
+            response = requests.get('https://ipinfo.io')
+            if response.status_code == 200:
+                data = response.json()
+                ipv4 = data.get('ip')
+                ipv6 = data.get('ip6')
+                return f'IPv4 Address: {ipv4}\nIPv6 Address: {ipv6}'
+            else:
+                return f'Error: {response.status_code}'
+        except Exception as d:
+            return f'Error: {str(d)}'
 
-        def get_ip_info():
-            try:
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        response = self.get_ip_info()
+        self.wfile.write(response.encode())
 
-                response = requests.get('https://ipinfo.io')
-
-                if response.status_code == 200:
-                    data = response.json()
-
-                    ipv4 = data.get('ip')
-                    ipv6 = data.get('ip6')
-
-                    print(f'IPv4 Address: {ipv4}')
-                    print(f'IPv6 Address: {ipv6}')
-                else:
-                    print(f'Error: {response.status_code}')
-            except Exception as d:
-                print(f'Error: {str(d)}')
-
-        if __name__ == '__main__':
-            get_ip_info()
-
-def run_vercel_server():
+def run_server():
     server_address = ("0.0.0.0", 3000)
     httpd = HTTPServer(server_address, IPCollectorHandler)
+    print("Server is running on port 3000")
     httpd.serve_forever()
 
-    if __name__ == "__main__":
-        run_vercel_server()
+if __name__ == "__main__":
+    run_server()
