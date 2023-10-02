ff17da4b3008a4f4de4c269d212941ccaa51f72a
C0083M <melsonc0083@my.uwstout.edu>
2023-10-02 19:05:17 +0000

    api

--- api/index.py
+++ api/index.py
@@ -1,30 +1,35 @@
 import requests
-from http.server import BaseHTTPRequestHandler
-from typing import Dict
 
-def get_ip_info() -> Dict[str, str]:
-    try:
-        response = requests.get('https://ipinfo.io')
-        if response.status_code == 200:
-            data = response.json()
-            ipv4 = data.get('ip')
-            ipv6 = data.get('ip6')
-            return {
-                "IPv4 Address": ipv4,
-                "IPv6 Address": ipv6
-            }
-        else:
-            return {
-                "Error": f"HTTP Error {response.status_code}"
-            }
-    except Exception as d:
-        return {
-            "Error": str(d)
-        }
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-def handler(event, context):
-    ip_info = get_ip_info()
-    return {
-        "statusCode": 200,
-        "body": ip_info
-    }
+class handler(BaseHTTPRequestHandler):
+
+
+        def get_ip_info():
+            try:
+
+                response = requests.get('https://ipinfo.io')
+
+                if response.status_code == 200:
+                    data = response.json()
+
+                    ipv4 = data.get('ip')
+                    ipv6 = data.get('ip6')
+
+                    print(f'IPv4 Address: {ipv4}')
+                    print(f'IPv6 Address: {ipv6}')
+                else:
+                    print(f'Error: {response.status_code}')
+            except Exception as d:
+                print(f'Error: {str(d)}')
+
+        if __name__ == '__main__':
+            get_ip_info()
+
+def server():
+    server_address = ("0.0.0.0", 3000)
+    httpd = HTTPServer(server_address, IPCollectorHandler)
+    httpd.server()
+
+    if __name__ == "__main__":
+        server()
