7144f92cb40380a084cbd9902b2af7c874445b0b
C0083M <melsonc0083@my.uwstout.edu>
2023-10-02 03:52:52 +0000

    api

--- api/index.py
+++ api/index.py
@@ -1,38 +1,40 @@
 import requests
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+{
+ "ipinfo": {
+        "api_key": "76fc0d7790488e"
+}
+}
+from http.server import BaseHTTPRequestHandler
 
-class IPCollectorHandler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            ip_info = self.get_public_ip_info()
-            self.wfile.write(ip_info.encode())
-        else:
-            self.send_response(404)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write("Not Found".encode())
+    #api_token = "76c0d7790488e"
 
-    def get_public_ip_info(self):
-        try:
-            response = requests.get('https://ipinfo.io')
-            if response.status_code == 200:
-                data = response.json()
-                ipv4 = data.get('ip')
-                ipv6 = data.get('ip6')
-                return f'IPv4 Address: {ipv4}\nIPv6 Address: {ipv6}'
-            else:
-                return f'Error: {response.status_code}'
-        except Exception as e:
-            return f'Error: {str(e)}'
+        def get_ip_info():
+            try:
 
-def run_vercel_server():
+                response = requests.get('https://ipinfo.io')
+
+                if response.status_code == 200:
+                    data = response.json()
+
+                    ipv4 = data.get('ip')
+                    ipv6 = data.get('ip6')
+
+                    print(f'IPv4 Address: {ipv4}')
+                    print(f'IPv6 Address: {ipv6}')
+                else:
+                    print(f'Error: {response.status_code}')
+            except Exception as d:
+                print(f'Error: {str(d)}')
+
+        if __name__ == '__main__':
+            get_ip_info()
+
+def server():
     server_address = ("0.0.0.0", 3000)
     httpd = HTTPServer(server_address, IPCollectorHandler)
-    httpd.serve_forever()
-
+    httpd.server_forever()
 if __name__ == "__main__":
-    run_vercel_server()
+    server
