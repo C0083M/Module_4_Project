ab2c3832dd9bacbf07bad8dd04090bd8b100fda9
C0083M <144258256+C0083M@users.noreply.github.com>
2023-10-17 13:04:36 -0500

    Update and rename test.bats to test.py

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+# test_index.py
+
+from index import function1, function2
+
+def test_function1_case1():
+    result = function1(input_value1)
+    assert result == expected_result1
+
+def test_function1_case2():
+    result = function1(input_value2)
+    assert result == expected_result2
+
+def test_function2_case1():
+    result = function2(input_value3)
+    assert result == expected_result3
+
