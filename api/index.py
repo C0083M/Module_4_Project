3de931ce5113484211d4fde3f0b2c87801e5bbe4
C0083M <melsonc0083@my.uwstout.edu>
2023-10-17 16:49:10 +0000

    moddified index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             ip_info = self.get_public_ip_info()
             self.wfile.write(ip_info.encode())
